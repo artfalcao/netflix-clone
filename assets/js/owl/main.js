e46d3cf6e45d330d786ae6592d75090d61b95d87
Arthur-Falcao <arthurmonteirofalcao1995@gmail.com>
2022-03-06 16:27:41 -0300

    adicionando funcionalidade nos botoes

--- assets/js/owl/main.js
+++ assets/js/owl/main.js
@@ -14,3 +14,11 @@ $('.owl-carousel').owlCarousel({
         }
     }
 })
+
+function redirecionarLer() {
+    window.location.assign('https://mangalivre.net/manga/naruto/1')
+}
+
+function redirecionarInfo() {
+    window.location.assign('https://pt.wikipedia.org/wiki/Naruto#:~:text=Naruto%20(%E3%83%8A%E3%83%AB%E3%83%88)%20%C3%A9%20uma%20s%C3%A9rie,ninja%20l%C3%ADder%20de%20sua%20vila.')
+}
